ae323c0a72ec400b24d38d673f0d9dc689550d10
jacobscheetz <scheetzj2@udayton.edu>
2020-04-19 17:28:57 -0400

    changed database to match new database

--- database.php
+++ database.php
@@ -1,12 +1,12 @@
 <?php
-	$mysqli = new mysqli('localhost', 'scheetzj2', 'mysecretP4$$', 'secad');
+	$mysqli = new mysqli('localhost', 'admin', 'P4$$w0rd', 'secad');
 	if ($mysqli->connect_error){
 		printf("Database connection failed : %s\n", $mysqli->connect_error);
 	exit();
 	}
 	function changepassword($username, $password){
 		global $mysqli;
-		$prepared_sql = "UPDATE users SET password=password(?) WHERE username= ?;";
+		$prepared_sql = "UPDATE users SET password=MD5(?) WHERE username= ?;";
 		echo "DEBUG>prepared_sql= $prepared_sql\n";
 		if (!$stmt = $mysqli->prepare($prepared_sql)) return FALSE;
 		$stmt->bind_param("ss", $newpassword, $username);
