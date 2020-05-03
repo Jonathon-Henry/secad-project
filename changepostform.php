f9b0e45232d4b00a37ff84c8323762416464db2d
JonHenry20 <Henryj14@udayton.edu>
2020-05-02 20:37:46 -0400

    adding button for deleting form, as well as many inputs for editing posts.

--- changepostform.php
+++ changepostform.php
@@ -11,21 +11,51 @@
   <title>Login page - SecAD</title>
 </head>
 <body>
-      	<h1>Change Password, MiniFacebook</h1>
+      	<h1>Change Post, MiniFacebook</h1>
 
 <?php
-  //some code here
-  echo "Current time: " . date("Y-m-d h:i:sa")
+  $postid = $_REQUEST["postid"];
+  $username = $_REQUEST["username"];
+  echo "Current time: " . date("Y-m-d h:i:sa");
+
+  $prepared_sql = "SELECT title, content, date FROM posts WHERE postid=$postid";
+
+	//create prepared statement from the string
+	if(!$stmt = $mysqli->prepare($prepared_sql))
+		echo "Prepared SQL Statement Error";
+
+	//execute the statement
+	if(!$stmt->execute())
+		echo "Execute error";
+
+	//Bind the results to variables
+	$title = NULL; $content = NULL; $date=NULL;
+	if(!$stmt->bind_result($title, $content, $date))
+		echo "Binding failed ";
+
+  if ($stmt->fetch()){
+  echo htmlentities($title) . ", " . htmlentities($content) . ", " .
+    htmlentities($date) . "<br>";
+  }
 ?>
-          <form action="changepassword.php" method="POST" class="form login">
-                Username: <?php echo htmlentities($_SESSION["username"]); ?><br>
+          <form action="changepost.php" method="POST" class="form login">
+                <input type="hidden" name="postid" value="<?php echo $postid; ?>" />
                 <input type="hidden" name="nocsrftoken" value="<?php echo $rand; ?>" />
-                New Password: <input type="password" class="text_field" name="newpassword" /> <br>
+                <input type="hidden" name="newdate" value="<?php echo date('Y-m-d h:i:sa'); ?>" />
+                Edit post title: <input class="text_field" name="newtitle" value="<?php echo $title; ?>" /> <br>
+                Edit post content: <input class="text_field" name="newcontent" value="<?php echo $content; ?>" /> <br>
                 <button class="button" type="submit">
-                  Change password
+                  Edit post
+                </button>
+          </form>
+          <form action="deletepost.php" method="POST" class="form login">
+                <input type="hidden" name="username" value="<?php echo $username; ?>" />
+                <input type="hidden" name="postid" value="<?php echo $postid; ?>" />
+                <input type="hidden" name="nocsrftoken" value="<?php echo $rand; ?>" />
+                <button class="button" type="submit">
+                  Delete post
                 </button>
           </form>
 
 </body>
 </html>
-
